8fbbf73fc81ccecd3b65189d12474292c6b1384c
Giles Cope <gilescope@gmail.com>
2025-09-23 09:55:59 +0100

    fix: update to latest alpine as suggested

--- earthfile2llb/with_docker_run_base.go
+++ earthfile2llb/with_docker_run_base.go
@@ -21,7 +21,7 @@ const (
 	dockerdWrapperPath          = "/var/earthly/dockerd-wrapper.sh"
 	dockerAutoInstallScriptPath = "/var/earthly/docker-auto-install.sh"
 	composeConfigFile           = "compose-config.yml"
-	suggestedDINDImage          = "earthbuild/dind:alpine-3.20-docker-26.1.5-r0"
+	suggestedDINDImage          = "earthbuild/dind:alpine-3.22-docker-28.3.3-r1"
 )
 
 // DockerLoadOpt holds parameters for WITH DOCKER --load parameter.

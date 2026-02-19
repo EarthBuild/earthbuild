d2dff97e0d0c0b58fbea1bc7d97569fe0ee49951
Giles Cope <gilescope@gmail.com>
2026-02-19 15:17:26 +0000

    fix: harden docker output reading
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- tests/wait-block/save-multi-platform-image-with-from/test.sh
+++ tests/wait-block/save-multi-platform-image-with-from/test.sh
@@ -5,7 +5,7 @@ set -uex
 export EARTHLY_VERSION_FLAG_OVERRIDES=""
 
 # clean up old images (best effort)
-docker images | grep earthly-multiplatform-wait-test-with-from | awk '{print $1 ":" $2}' | xargs -r -n 1 docker rmi
+docker images --format '{{.Repository}}:{{.Tag}}' | grep earthly-multiplatform-wait-test-with-from | xargs -r -n 1 docker rmi
 
 cd "$(dirname "$0")"
 

34a5b66aedac7606d79a8f2af71f9accf2783373
Giles Cope <gilescope@gmail.com>
2026-04-11 08:09:19 +0100

    fix: add blank line to satisfy wsl_v5 linter
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- cmd/earthly/main.go
+++ cmd/earthly/main.go
@@ -59,6 +59,7 @@ func main() {
 	// between earthly client and buildkitd during the upgrade transition.
 	// TODO: remove once all released buildkitd images use grpc-go >= 1.67
 	_ = os.Setenv("GRPC_ENFORCE_ALPN_ENABLED", "false")
+
 	os.Exit(run())
 }
 

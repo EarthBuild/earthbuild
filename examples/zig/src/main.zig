9bf68bd9660443e1c3ca850ea68a96aea267668b
Janis Horsts <janis.horsts@gmail.com>
2026-05-07 10:25:38 +0100

    chore: tidy

--- examples/zig/src/main.zig
+++ examples/zig/src/main.zig
@@ -2,28 +2,28 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(init.arena.allocator());
+    var stdout = std.Io.File.stdout().writer(init.io, &.{});
+    var stderr = std.Io.File.stderr().writer(init.io, &.{});
 
     const argLen = args.len;
     if (argLen > 2) {
-        std.debug.print("too many arguments\n", .{});
+        try stderr.interface.print("too many arguments\n", .{});
         return;
     } else if (argLen < 2) {
-        std.debug.print("too few arguments\n", .{});
+        try stderr.interface.print("too few arguments\n", .{});
         return;
     }
 
     const to = try std.fmt.parseInt(usize, args[1], 10);
 
-    for (0..to) |num| {
+    for (1..to + 1) |num| {
         const result = try fizzBuzz(init.arena.allocator(), num);
-        std.debug.print("{s}\n", .{result});
+        try stdout.interface.print("{s}\n", .{result});
     }
 }
 
 fn fizzBuzz(allocator: std.mem.Allocator, num: usize) ![]const u8 {
-    if (num == 0) {
-        return try std.fmt.allocPrint(allocator, "{d}", .{num});
-    } else if (num % 15 == 0) {
+    if (num % 15 == 0) {
         return "fizzbuzz";
     } else if (num % 3 == 0) {
         return "fizz";
@@ -35,16 +35,12 @@ fn fizzBuzz(allocator: std.mem.Allocator, num: usize) ![]const u8 {
 }
 
 test {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var arena = std.heap.ArenaAllocator.init(allocator);
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
 
-    try std.testing.expectEqualStrings("0", try fizzBuzz(arena.allocator(), 0));
+    try std.testing.expectEqualStrings("1", try fizzBuzz(arena.allocator(), 1));
+    try std.testing.expectEqualStrings("2", try fizzBuzz(arena.allocator(), 2));
     try std.testing.expectEqualStrings("fizz", try fizzBuzz(arena.allocator(), 3));
     try std.testing.expectEqualStrings("buzz", try fizzBuzz(arena.allocator(), 5));
     try std.testing.expectEqualStrings("fizzbuzz", try fizzBuzz(arena.allocator(), 15));
-    try std.testing.expectEqualStrings("2", try fizzBuzz(arena.allocator(), 2));
 }

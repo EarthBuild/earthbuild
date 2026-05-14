89d9093935cc5a84fdef8ffce92e7bed6bf0c19f
Janis Horsts <janis.horsts@gmail.com>
2026-05-14 21:35:00 +0100

    Update dedup.go

--- states/dedup/dedup.go
+++ states/dedup/dedup.go
@@ -1,3 +1,3 @@
-// Package dedup provides target and build arg deduplication for Earthly's build states,
+// Package dedup provides target and build arg deduplication for EarthBuild's build states,
 // ensuring identical targets are not evaluated multiple times.
 package dedup

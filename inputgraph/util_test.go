b82d4472716126d949fb1a05bf7c3776684c5309
Janis Horsts <janis.horsts@gmail.com>
2026-07-01 18:57:01 +0100

    refactor: read color mode from NO_COLOR and FORCE_COLOR (#623)
    * refactor: read color mode from NO_COLOR and FORCE_COLOR
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: tidy
    
    * chore: simplify color.NoColor assignment
    
    * refactor: rely on color.NoColor only
    
    * fix: go run ./cmd/earthly

--- inputgraph/util_test.go
+++ inputgraph/util_test.go
@@ -27,7 +27,7 @@ func TestParseProjectCommand(t *testing.T) {
 	}
 
 	ctx := context.Background()
-	cons := conslogging.New(os.Stderr, &sync.Mutex{}, conslogging.NoColor, 0, conslogging.Info, false)
+	cons := conslogging.New(os.Stderr, &sync.Mutex{}, 0, conslogging.Info, false)
 
 	org, project, err := ParseProjectCommand(ctx, target, cons)
 	r.NoError(err)
@@ -45,7 +45,7 @@ func TestParseProjectCommandNoProject(t *testing.T) {
 	}
 
 	ctx := context.Background()
-	cons := conslogging.New(os.Stderr, &sync.Mutex{}, conslogging.NoColor, 0, conslogging.Info, false)
+	cons := conslogging.New(os.Stderr, &sync.Mutex{}, 0, conslogging.Info, false)
 
 	_, _, err := ParseProjectCommand(ctx, target, cons)
 	r.Error(err)

4c1104b7768438ca45bdfa32ac9cc477dc5bcd38
Janis Horsts <janis.horsts@gmail.com>
2026-07-05 20:45:49 +0100

    chore: tidy

--- util/statsstreamparser/parser.go
+++ util/statsstreamparser/parser.go
@@ -4,15 +4,20 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 
 	"github.com/containerd/go-runc"
 )
 
 // Parser parses stream data containing execution statistics.
 type Parser struct {
-	buf                 *bytes.Buffer
-	readProtocolVersion bool
+	// buf accumulates incoming stream data across multiple Parse calls.
+	buf *bytes.Buffer
+	// hasReadVersion is true if the protocol version byte for the current packet
+	// has been read and validated, but the stats payload is still pending.
+	hasReadVersion bool
 }
 
 // New creates a new parser instance.
@@ -32,38 +37,56 @@ func (ssp *Parser) Parse(b []byte) ([]*runc.Stats, error) {
 	var stats []*runc.Stats
 
 	for {
-		if !ssp.readProtocolVersion {
-			if ssp.buf.Len() < 1 {
-				break
+		if !ssp.hasReadVersion {
+			var protocolVersion byte
+
+			protocolVersion, err = ssp.buf.ReadByte()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				return nil, err
 			}
-			protocolVersion, _ := ssp.buf.ReadByte()
+
 			if protocolVersion != 1 {
 				return nil, fmt.Errorf("unexpected stats stream protocol version %d", protocolVersion)
 			}
 
-			ssp.readProtocolVersion = true
+			ssp.hasReadVersion = true
 		}
 
-		if ssp.buf.Len() < 4 {
-			break
-		}
-		length := binary.LittleEndian.Uint32(ssp.buf.Bytes()[:4])
-		if ssp.buf.Len() < 4+int(length) {
-			break
+		lenBytes, err := ssp.buf.Peek(4)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, err
 		}
 
-		ssp.buf.Next(4)
-		statsStreamBytes := ssp.buf.Next(int(length))
+		n := int(binary.LittleEndian.Uint32(lenBytes))
+
+		statsBytes, err := ssp.buf.Peek(4 + n)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, err
+		}
+
+		ssp.buf.Next(4 + n)
 
 		var runcStat runc.Stats
 
-		err = json.Unmarshal(statsStreamBytes, &runcStat)
+		err = json.Unmarshal(statsBytes[4:], &runcStat)
 		if err != nil {
 			return nil, err
 		}
 
 		stats = append(stats, &runcStat)
-		ssp.readProtocolVersion = false
+		ssp.hasReadVersion = false
 	}
 
 	return stats, nil

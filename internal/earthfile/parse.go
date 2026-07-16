993d659c21a228de55deb337ff7f9db8145b01de
Giles Cope <gilescope@gmail.com>
2026-07-16 09:44:01 +0100

    fix: strip line continuations inside quoted strings
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- internal/earthfile/parse.go
+++ internal/earthfile/parse.go
@@ -1667,8 +1667,14 @@ func replaceEscape(str string) string {
 
 		switch {
 		case c == '\\':
-			switch {
-			case inDouble:
+			// Line continuations are stripped regardless of quote state:
+			// Earthfile continuations are resolved before the shell ever
+			// sees the command (the pre-0.8.18 antlr parser behaved the
+			// same), so a trailing backslash inside quotes joins lines
+			// rather than surviving as a literal.
+			if n := matchLineContinuationFrom(str, i); n > 0 {
+				i += n
+			} else {
 				sb.WriteByte(c)
 
 				i++
@@ -1677,32 +1683,6 @@ func replaceEscape(str string) string {
 					sb.WriteByte(str[i])
 
 					i++
-				}
-
-			case inSingle:
-				sb.WriteByte(c)
-
-				i++
-
-				if i < len(str) {
-					sb.WriteByte(str[i])
-
-					i++
-				}
-
-			default:
-				if n := matchLineContinuationFrom(str, i); n > 0 {
-					i += n
-				} else {
-					sb.WriteByte(c)
-
-					i++
-
-					if i < len(str) {
-						sb.WriteByte(str[i])
-
-						i++
-					}
 				}
 			}
 
